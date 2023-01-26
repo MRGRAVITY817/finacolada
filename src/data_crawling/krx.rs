dd7352b0d5efae1a6b1903b43073098984828840
Hoon Wee <mrgravity817@gmail.com>
2023-01-26 16:41:19 +0900

    test: can crawl more than twice with same otp

--- src/data_crawling/krx.rs
+++ src/data_crawling/krx.rs
@@ -71,18 +71,21 @@ pub async fn get_sector_data(otp: &str, query_client: &Client) -> Result<String,
 #[cfg(test)]
 mod test {
     use {super::*, insta::*, reqwest};
+    const TEST_TRADING_DATE: &'static str = "20230120";
 
     #[tokio::test]
     async fn generate_otp_for_kospi_sector_data() {
         // Arrange
-        let trading_date = "20210108";
         let client = reqwest::Client::new();
-        let market_type = MarketType::Kospi;
-        let info_type = InfoType::Sector;
         // Act
-        let result = generate_krx_otp(&client, info_type, market_type, trading_date)
-            .await
-            .unwrap();
+        let result = generate_krx_otp(
+            &client,
+            InfoType::Sector,
+            MarketType::Kospi,
+            TEST_TRADING_DATE,
+        )
+        .await
+        .unwrap();
         // Assert
         assert!(result.len() > 10)
     }
@@ -90,14 +93,16 @@ mod test {
     #[tokio::test]
     async fn generate_otp_for_kosdaq_sector_data() {
         // Arrange
-        let trading_date = "20210108";
         let client = reqwest::Client::new();
-        let market_type = MarketType::Kosdaq;
-        let info_type = InfoType::Sector;
         // Act
-        let result = generate_krx_otp(&client, info_type, market_type, trading_date)
-            .await
-            .unwrap();
+        let result = generate_krx_otp(
+            &client,
+            InfoType::Sector,
+            MarketType::Kosdaq,
+            TEST_TRADING_DATE,
+        )
+        .await
+        .unwrap();
         // Assert
         assert!(result.len() > 10)
     }
@@ -105,14 +110,16 @@ mod test {
     #[tokio::test]
     async fn generate_otp_for_kospi_individual_data() {
         // Arrange
-        let trading_date = "20210108";
         let client = reqwest::Client::new();
-        let market_type = MarketType::Kospi;
-        let info_type = InfoType::Individual;
         // Act
-        let result = generate_krx_otp(&client, info_type, market_type, trading_date)
-            .await
-            .unwrap();
+        let result = generate_krx_otp(
+            &client,
+            InfoType::Individual,
+            MarketType::Kospi,
+            TEST_TRADING_DATE,
+        )
+        .await
+        .unwrap();
         // Assert
         assert!(result.len() > 10)
     }
@@ -120,14 +127,16 @@ mod test {
     #[tokio::test]
     async fn generate_otp_for_kosdaq_individual_data() {
         // Arrange
-        let trading_date = "20210108";
         let client = reqwest::Client::new();
-        let market_type = MarketType::Kosdaq;
-        let info_type = InfoType::Individual;
         // Act
-        let result = generate_krx_otp(&client, info_type, market_type, trading_date)
-            .await
-            .unwrap();
+        let result = generate_krx_otp(
+            &client,
+            InfoType::Individual,
+            MarketType::Kosdaq,
+            TEST_TRADING_DATE,
+        )
+        .await
+        .unwrap();
         // Assert
         assert!(result.len() > 10)
     }
@@ -138,8 +147,7 @@ mod test {
         let client = reqwest::Client::new();
         let info_type = InfoType::Sector;
         let market_type = MarketType::Kospi;
-        let trading_date = "20210108";
-        let otp = generate_krx_otp(&client, info_type, market_type, trading_date)
+        let otp = generate_krx_otp(&client, info_type, market_type, TEST_TRADING_DATE)
             .await
             .unwrap();
         // Act
@@ -154,13 +162,33 @@ mod test {
         let client = reqwest::Client::new();
         let info_type = InfoType::Sector;
         let market_type = MarketType::Kosdaq;
-        let trading_date = "20210108";
-        let otp = generate_krx_otp(&client, info_type, market_type, trading_date)
+        let otp = generate_krx_otp(&client, info_type, market_type, TEST_TRADING_DATE)
             .await
             .unwrap();
         // Act
         let result = get_sector_data(&otp, &client).await.unwrap();
         // Assert
         assert_yaml_snapshot!(result)
+    }
+
+    #[tokio::test]
+    async fn download_sector_data_twice_with_same_otp() {
+        // Arrange
+        let client = reqwest::Client::new();
+        let otp = generate_krx_otp(
+            &client,
+            InfoType::Sector,
+            MarketType::Kospi,
+            TEST_TRADING_DATE,
+        )
+        .await
+        .unwrap();
+        // Act
+        let first_result = get_sector_data(&otp, &client).await.unwrap();
+        let second_result = get_sector_data(&otp, &client).await.unwrap();
+        // Assert
+        assert_yaml_snapshot!(first_result);
+        assert_yaml_snapshot!(second_result);
+        assert_eq!(first_result, second_result)
     }
 }
