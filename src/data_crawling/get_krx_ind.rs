9e3b8eb332debb8adda9b377cc7d36a2ec2ff014
Hoon Wee <mrgravity817@gmail.com>
2023-01-26 10:36:02 +0900

    test: get_sector_data works for kosdaq

--- src/data_crawling/get_krx_ind.rs
+++ src/data_crawling/get_krx_ind.rs
@@ -96,4 +96,19 @@ mod test {
         // Assert
         assert_yaml_snapshot!(result)
     }
+
+    #[tokio::test]
+    async fn download_sector_data_from_krx_for_kosdaq() {
+        // Arrange
+        let trading_date = "20210108";
+        let client = reqwest::Client::new();
+        let market_type = MarketType::Kosdaq;
+        let otp = get_krx_otp(trading_date, market_type, &client)
+            .await
+            .unwrap();
+        // Act
+        let result = get_sector_data(&otp, &client).await.unwrap();
+        // Assert
+        assert_yaml_snapshot!(result)
+    }
 }
