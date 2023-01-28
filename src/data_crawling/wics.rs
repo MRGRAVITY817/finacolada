fd37dcdef26b8dfa7b78b10d29469222b5eb8d7c
Hoon Wee <mrgravity817@gmail.com>
2023-01-29 00:35:32 +0900

    feat: get daily wics data as row

--- src/data_crawling/wics.rs
+++ src/data_crawling/wics.rs
@@ -1,3 +1,26 @@
+use serde_json::Value;
+
+use {super::biz_day::get_latest_biz_day, serde::Deserialize};
+
+#[derive(Deserialize, Debug)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub struct WicsRow {
+    idx_cd: String,
+    idx_nm_kor: String,
+    all_mkt_val: u64,
+    cmp_cd: String,
+    cmp_kor: String,
+    mkt_val: u64,
+    wgt: f32,
+    s_wgt: f32,
+    cal_wgt: f32,
+    sec_cd: String,
+    sec_nm_kor: String,
+    seq: i32,
+    top60: u32,
+    apt_shr_cnt: u64,
+}
+
 pub const SECTOR_CODE_LIST: [&str; 10] = [
     "G25", "G35", "G50", "G40", "G10", "G20", "G55", "G30", "G15", "G45",
 ];
@@ -18,18 +41,55 @@ pub async fn get_wics(
         .await
 }
 
-pub async fn get_daily_wics_list(query_client: &reqwest::Client, date: &str) -> anyhow::Result<()> {
-    std::fs::create_dir_all(format!("assets/wics/{date}"))?;
-    for sector in SECTOR_CODE_LIST {
-        let result = get_wics(query_client, date, sector).await?;
-        std::fs::write(format!("assets/wics/{date}/{sector}.json"), result)?;
+pub async fn get_daily_wics_list(
+    query_client: &reqwest::Client,
+    date: &str,
+) -> anyhow::Result<Vec<WicsRow>> {
+    let mut row_list: Vec<WicsRow> = vec![];
+    for sector_code in SECTOR_CODE_LIST {
+        let json_string = get_wics(query_client, date, sector_code).await?;
+        let meta: Value = serde_json::from_str(&json_string)?;
+        for wics in meta["list"].as_array().unwrap() {
+            let item: WicsRow = serde_json::from_value(wics.to_owned())?;
+            row_list.push(item);
+        }
     }
-    Ok(())
+
+    Ok(row_list)
 }
+
+// pub async fn get_latest_wisc_data(query_client: &reqwest::Client) -> anyhow::Result<String> {
+//     // 1. Get WICS data for every sector codes
+//     let date = get_latest_biz_day(query_client).await?;
+//     // 2. Deserialize JSON string as struct
+//     let meta: Value = serde_json::from_str(&)
+//     // 3. Get the "list" property
+//     // 4. Create a DataFrame that contains each sector items.
+//     let df = df![
+//       "IDX_CD" => idx_cd,
+//       "IDX_NM_KOR" => idx_nm_kor,
+//       "ALL_MKT_VAL" => all_mkt_val,
+//       "CMP_CD" => cmp_cd,
+//       "CMP_KOR" => cmp_kor,
+//       "MKT_VAL" => mkt_val,
+//       "WGT" => wgt,
+//       "S_WGT" => s_wgt,
+//       "CAL_WGT" => cal_wgt,
+//       "SEC_CD" => sec_cd,
+//       "SEC_NM_KOR" => sec_nm_kor,
+//       "SEQ" => seq,
+//       "TOP60" => top60,
+//       "APT_SHR_CNT" => apt_shr_cnt
+//     ]?;
+//     // 5. Save DataFrame as parquet file
+//     let file_name = "assets/wics/latest_wics_data.parquet";
+//     // 6. Return the parquet file name
+//     Ok(file_name.to_string())
+// }
 
 #[cfg(test)]
 mod test {
-    use {super::*, insta::assert_snapshot};
+    use {super::*, insta::assert_yaml_snapshot};
 
     #[tokio::test]
     async fn should_have_json_text() {
@@ -40,7 +100,7 @@ mod test {
         // Act
         let result = get_wics(&client, date, sector_code).await.unwrap();
         // Assert
-        assert_snapshot!(result)
+        assert_yaml_snapshot!(result)
     }
 
     #[tokio::test]
@@ -55,5 +115,16 @@ mod test {
         let now_result = get_wics(&client, now_date, sector_code).await.unwrap();
         // Assert
         assert_ne!(early_result, now_result)
+    }
+
+    #[tokio::test]
+    async fn gets_wics_rows() {
+        // Arrange
+        let client = reqwest::Client::new();
+        let date = "20230126";
+        // Act
+        let result = get_daily_wics_list(&client, date).await.unwrap();
+        // Assert
+        assert_yaml_snapshot!(format!("{:?}", result.iter().next()))
     }
 }
