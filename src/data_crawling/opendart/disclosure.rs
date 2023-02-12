0a84ffa42089a783b40d260cbd33977a1828d5c6
Hoon Wee <mrgravity817@gmail.com>
2023-02-12 23:25:19 +0900

    feat: get single account info

--- src/data_crawling/opendart/disclosure.rs
+++ src/data_crawling/opendart/disclosure.rs
@@ -69,7 +69,7 @@ mod test {
                 .await
                 .unwrap();
 
-        assert_snapshot!(result, @r###"{"status":"000","message":"정상","page_no":1,"page_count":3,"total_count":15232,"total_page":5078,"list":[{"corp_code":"01688425","corp_name":"케이비오토제오차유동화전문유한회사","stock_code":"","corp_cls":"E","report_nm":"[기재정정]자산유동화계획의등록신청서[신탁회사(은행신탁계정포함)]","rcept_no":"20230208000631","flr_nm":"한국씨티은행","rcept_dt":"20230208","rm":""},{"corp_code":"01688425","corp_name":"케이비오토제오차유동화전문유한회사","stock_code":"","corp_cls":"E","report_nm":"[기재정정]자산유동화계획의등록신청서[신탁회사(은행신탁계정포함)]","rcept_no":"20230207000457","flr_nm":"한국씨티은행","rcept_dt":"20230207","rm":"정"},{"corp_code":"01688425","corp_name":"케이비오토제오차유동화전문유한회사","stock_code":"","corp_cls":"E","report_nm":"[기재정정]자산유동화계획의등록신청서[신탁회사(은행신탁계정포함)]","rcept_no":"20230127000459","flr_nm":"한국씨티은행","rcept_dt":"20230127","rm":"정"}]}"###)
+        assert_snapshot!(result, @r###"{"status":"000","message":"정상","page_no":1,"page_count":3,"total_count":15263,"total_page":5088,"list":[{"corp_code":"00241388","corp_name":"디비자산운용","stock_code":"","corp_cls":"E","report_nm":"[기재정정]증권신고서(집합투자증권-신탁형)(DB지수연계더블플러스증권투자신탁SEK-37호[ELS-파생형])","rcept_no":"20230208000449","flr_nm":"디비자산운용","rcept_dt":"20230208","rm":""},{"corp_code":"00241388","corp_name":"디비자산운용","stock_code":"","corp_cls":"E","report_nm":"증권신고서(집합투자증권-신탁형)(DB지수연계더블플러스증권투자신탁SEK-37호[ELS-파생형])","rcept_no":"20230127000654","flr_nm":"디비자산운용","rcept_dt":"20230127","rm":"정"},{"corp_code":"00241388","corp_name":"디비자산운용","stock_code":"","corp_cls":"E","report_nm":"[기재정정]증권신고서(집합투자증권-신탁형)(DB지수연계더블플러스증권투자신탁SNE-8호[ELS-파생형])","rcept_no":"20230208000070","flr_nm":"디비자산운용","rcept_dt":"20230208","rm":""}]}"###)
     }
 
     #[tokio::test]
