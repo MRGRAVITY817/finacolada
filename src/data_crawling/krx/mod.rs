778a9da2676f4d8eef843001921d9fa4ef9f4d4e
Hoon Wee <mrgravity817@gmail.com>
2023-02-15 22:16:46 +0900

    feat: get latest krx data

--- src/data_crawling/krx/mod.rs
+++ src/data_crawling/krx/mod.rs
@@ -1,6 +1,102 @@
+use {
+    self::{
+        download::generate_krx_otp,
+        merge_table::{merge_kospi_kosdaq, merge_sector_individual},
+        save::{convert_individual_xlsx_to_parquet, convert_sector_xlsx_to_parquet},
+    },
+    crate::data_crawling::krx::download::download_krx_data,
+};
+
 pub mod download;
 pub mod merge_table;
 pub mod save;
+
+pub async fn get_latest_krx_data(
+    query_client: &reqwest::Client,
+    trading_date: &str,
+) -> anyhow::Result<()> {
+    let kospi_sector_otp = generate_krx_otp(
+        query_client,
+        InfoType::Sector,
+        MarketType::Kospi,
+        trading_date,
+    )
+    .await?;
+    let kospi_individual_otp = generate_krx_otp(
+        query_client,
+        InfoType::Individual,
+        MarketType::Kospi,
+        trading_date,
+    )
+    .await?;
+    let kosdaq_sector_otp = generate_krx_otp(
+        query_client,
+        InfoType::Sector,
+        MarketType::Kosdaq,
+        trading_date,
+    )
+    .await?;
+    let kosdaq_individual_otp = generate_krx_otp(
+        query_client,
+        InfoType::Individual,
+        MarketType::Kosdaq,
+        trading_date,
+    )
+    .await?;
+
+    tokio::try_join!(
+        download_krx_data(
+            query_client,
+            &kospi_sector_otp,
+            "assets/krx/kospi_sector.xlsx"
+        ),
+        download_krx_data(
+            query_client,
+            &kospi_individual_otp,
+            "assets/krx/kospi_individual.xlsx"
+        ),
+        download_krx_data(
+            query_client,
+            &kosdaq_sector_otp,
+            "assets/krx/kosdaq_sector.xlsx"
+        ),
+        download_krx_data(
+            query_client,
+            &kosdaq_individual_otp,
+            "assets/krx/kosdaq_individual.xlsx"
+        ),
+    )?;
+
+    convert_sector_xlsx_to_parquet(
+        "assets/krx/kospi_sector.xlsx",
+        "assets/krx/kospi_sector.parquet",
+    )?;
+    convert_individual_xlsx_to_parquet(
+        "assets/krx/kospi_individual.xlsx",
+        "assets/krx/kospi_individual.parquet",
+    )?;
+    convert_sector_xlsx_to_parquet(
+        "assets/krx/kosdaq_sector.xlsx",
+        "assets/krx/kosdaq_sector.parquet",
+    )?;
+    convert_individual_xlsx_to_parquet(
+        "assets/krx/kosdaq_individual.xlsx",
+        "assets/krx/kosdaq_individual.parquet",
+    )?;
+
+    merge_sector_individual(
+        "assets/krx/kospi_sector.parquet",
+        "assets/krx/kospi_individual.parquet",
+        "assets/krx/kospi_sector_individual.parquet",
+    )?;
+    merge_sector_individual(
+        "assets/krx/kosdaq_sector.parquet",
+        "assets/krx/kosdaq_individual.parquet",
+        "assets/krx/kosdaq_sector_individual.parquet",
+    )?;
+
+    Ok(())
+}
 
 pub enum InfoType {
     Sector,
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use data_crawling::{biz_day::get_latest_biz_day, krx::get_latest_krx_data};
+
 mod data_crawling;
 mod utils;
 
-fn main() {}
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let query_client = reqwest::Client::new();
+    let trading_date = get_latest_biz_day(&query_client).await?;
+
+    get_latest_krx_data(&query_client, &trading_date).await
+}
